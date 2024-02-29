8afc45e309f99b5fa091ac8f29a96090d67e8b1e
Mingyeol Kim <neomingyeol@gmail.com>
2024-02-29 23:59:10 +0900

    palyer dev

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -35,34 +35,6 @@ public class Player : MonoBehaviour
         }
     }
 
-    private IEnumerator Co_PlayerMove(Vector2Int Direction)
-    {
-        isMoving = true;
-
-        Vector3Int myCellPosition = tilemap.WorldToCell(transform.position); // 현재 위치의 셀 좌표를 가져옴
-
-        Vector3Int targetCell = myCellPosition + new Vector3Int(Direction.x, Direction.y, 0); // 이동할 셀의 위치
-
-        Vector3 targetPos = tilemap.GetCellCenterWorld(targetCell); // 이동할 셀의 중심 위치를 가져옴
-
-        float distance = Vector3.Distance(transform.position, targetPos); // 목표 지점까지의 거리
-
-        float duration = distance / moveSpeed;  // 목표 지점까지 도달하는 데 걸리는 시간 = 목표 지점 간 거리 / 이동 속도
-
-        float elapsedTime = 0f;
-
-        while (elapsedTime < duration)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        transform.position = targetPos; // 정확한 위치로 이동
-
-        isMoving = false;
-    }
-
     private void MoveRight()
     {
         StartCoroutine(Co_PlayerMove(Vector2Int.right));
@@ -82,4 +54,37 @@ public class Player : MonoBehaviour
     {
         StartCoroutine(Co_PlayerMove(Vector2Int.down));
     }
+
+    private IEnumerator Co_PlayerMove(Vector2Int Direction)
+    {
+        isMoving = true;
+
+        // 현재 위치의 셀 좌표를 가져옴
+        Vector3Int myCellPosition = tilemap.WorldToCell(transform.position);
+
+        // 이동할 셀의 위치
+        Vector3Int targetCell = myCellPosition + new Vector3Int(Direction.x, Direction.y, 0); 
+
+        // 이동할 셀의 중심 위치를 가져옴
+        Vector3 targetPos = tilemap.GetCellCenterWorld(targetCell); 
+
+        // 목표 지점까지의 거리
+        float distance = Vector3.Distance(transform.position, targetPos); 
+
+        // 목표 지점까지 도달하는 데 걸리는 시간 = 목표 지점 간 거리 / 이동 속도
+        float duration = distance / moveSpeed;  
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos; // 정확한 위치로 이동
+
+        isMoving = false;
+    }
+
 }
